48ff7a48b314dfc0c447b06eb61395226cfed145
Attah Christian <tuchris02@gmail.com>
2023-08-01 13:50:30 +0100

    modified settings.py

--- BlueOcean/settings.py
+++ BlueOcean/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-boui4vdt4io7aewgv_$vcl-vj=rct0g#na49dddlu1ci25gnv*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['aqueous-dusk-52739-ae2f12d6332a.herokuapp.com', 'koinspot.com', 'www.koinspot.com']
+ALLOWED_HOSTS = ['.aqueous-dusk-52739-ae2f12d6332a.herokuapp.com', 'koinspot.com', 'www.koinspot.com']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
