5ed86f54b5a9f6d0b7c0a41842299a64c6d862cf
Attah Christian <tuchris02@gmail.com>
2023-08-02 12:28:56 +0100

    modified settings.py

--- BlueOcean/settings.py
+++ BlueOcean/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-boui4vdt4io7aewgv_$vcl-vj=rct0g#na49dddlu1ci25gnv*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['aqueous-dusk-52739-ae2f12d6332a.herokuapp.com','.vercel.app']
+ALLOWED_HOSTS = ["localhost", "192.168.0.172", "127.0.0.1"]
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
