fab4b755f3d13d03b2828ee874a8f76b47c2b143
Attah Christian <tuchris02@gmail.com>
2023-08-02 12:40:09 +0100

    modified settings.py

--- BlueOcean/settings.py
+++ BlueOcean/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-boui4vdt4io7aewgv_$vcl-vj=rct0g#na49dddlu1ci25gnv*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "192.168.0.172", "127.0.0.1"]
+ALLOWED_HOSTS = ["aqueous-dusk-52739-ae2f12d6332a.herokuapp.com","localhost", "192.168.0.172", "127.0.0.1"]
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
